a84b433a5b39e6261176dcf34aee25ca9dcd603a
MaryMagnolia <babyhype99@gmail.com>
2018-07-20 01:10:15 +0000

    getting caught up

---
+++ Drop/Students/Elise_Magnolia/TimTurtle.py
@@ -0,0 +1,30 @@
+import turtle #this pulls the turtle library
+window = turtle.Screen() #gets a screen
+Tim = turtle.Turtle() #makes a turtle and assigns it to the name Tim
+
+#stem
+Tim.color("green", "black")
+Tim.left(90) 
+Tim.forward(100)
+
+#Center
+Tim.color("black", "black)
+Tim.begin_fill()          
+Tim.right(90)
+Tim.circle(10)
+        
+#Petals
+for i in range(1,244):
+    if (Tim.color() == ("red", "black")):
+        Tim.color("orange", "black")
+    elif (
+
+    Tim.left(15)
+    Tim.forward(50)
+    Tim.left(157)
+    Tim.forward(50)
+
+
+
+
+window.exitonclick()
